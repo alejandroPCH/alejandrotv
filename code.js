7fa460b46d3fa03b6ec285c6c07ea33cbe78388e
alejandro <alejandropachas1@gmail.com>
2021-05-08 13:26:42 -0500

    images were added and javascript improved

--- code.js
+++ code.js
@@ -1,19 +1,33 @@
-const item=document.querySelector(".container__item")
+const item=document.querySelector(".carousel__container")
 const text=document.querySelector(".presentation-text__item")
-
+const main_image=document.querySelector(".main_image")
 
 item.addEventListener('mouseover',grow_item)
 item.addEventListener('mouseout',remove_item)
 item.addEventListener('click', show_item)
 
-function show_item(){
+
+function show_item(e){
+  // main_image.style.backgroundImage=e.target.id
+  // .setAttribute("style", "background-image: url("
+  style = e.target.currentStyle || window.getComputedStyle(e.target, false),
+  bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+
+   main_image.style.backgroundImage=`url(${bi})`
+   
+  main_image.style.animation = 'none';
+  main_image.offsetHeight; /* trigger reflow */
+  main_image.style.animation = null; 
+
+
 
   text.textContent=`Esta escena es una de mis favoritas pues para mi, es como el inicio del camino de un heroe`
+
+  
 }
 
 function grow_item(e){
 
-  console.log(e.target.id)
 
   
 item.classList.add('display')
